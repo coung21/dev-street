363ebc511806692cb84a19457b5e450ca3a323a0
Duong Cong Cuong <120638231+CoungDev@users.noreply.github.com>
2023-05-29 08:17:33 +0700

    update user schema

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -6,12 +6,26 @@ const DOCUMENT = 'User';
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, require: true },
+    name: { type: String },
     email: { type: String, require: true, unique: true },
     password: { type: String, allowNull: true, default: null },
     avatar: {
       type: String,
       default: 'https://www.drupal.org/files/issues/default-avatar.png',
     },
+    bio: { type: String },
+    links: { type: String },
+    joinDate: { type: Date, default: Date.now() },
+    skills: { type: String },
+    location: { type: String },
+    work: {type: String},
+    education:{type: String},
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+    followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    following: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    followedTags : [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
+    bookmarked: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
     verificationCode: { type: String, allowNull: true },
     isVerify: {
       type: Boolean,
@@ -26,8 +40,8 @@ const userSchema = new mongoose.Schema(
       default: 'Email',
     },
     googleId: { type: String },
-    resetPasswordToken: { type: String, allowNull: true},
-    resetPasswordExpires: {type: Date, allowNull: true},
+    resetPasswordToken: { type: String, allowNull: true },
+    resetPasswordExpires: { type: Date, allowNull: true },
   },
   {
     collection: COLLECTION,

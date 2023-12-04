7a2dafe21f4d0c8eb318a527b0a6676d7ebbdf08
coungdev <coungdev@gmail.com>
2023-12-05 00:02:35 +0700

    finish

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './spinner.css'
+
+function Spinner() {
+  return (
+    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+  )
+}
+
+export default Spinner
